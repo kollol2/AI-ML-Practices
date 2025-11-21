c733095c4e323ba26090ec729d0ac78316621d4b
kollol2 <kollolmondal987654321@gmail.com>
2025-11-21 19:13:12 +0530

    new code added

---
+++ celcius to fren.py
@@ -0,0 +1,6 @@
+celsius = input("Enter temperature in Celsius: ")   # string input
+celsius = float(celsius)                            # convert to float
+
+fahrenheit = (celsius * (9/5)) + 32                 # apply formula
+
+print("Temperature in Fahrenheit =", fahrenheit)
