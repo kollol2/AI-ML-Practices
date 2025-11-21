f0159b219516a6a434a0b38e2852b400a569974d
kollol2 <kollolmondal987654321@gmail.com>
2025-11-21 12:02:06 +0530

    type casting

---
+++ string-to-int-float.py
@@ -0,0 +1,14 @@
+num_str = input("Enter a number (string format): ")
+
+# convert to integer
+num_int = int(num_str)
+
+# convert to float
+num_float = float(num_str)
+
+# convert back to string
+num_string_again = str(num_int)
+
+print(num_int, type(num_int))
+print(num_float, type(num_float))
+print(num_string_again, type(num_string_again))
