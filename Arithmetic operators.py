5dff3b7b869223c3f07a6ae8bb19ebc1bb7a12bf
kollol2 <kollolmondal987654321@gmail.com>
2025-11-21 11:30:41 +0530

    difference,product,quotient

--- Arithmetic operators.py
+++ Arithmetic operators.py
@@ -5,3 +5,21 @@ num1 = int (input("Enter your first number :"))
 num2 = int (input("Enter your second number :"))
 total = num1 + num2
 print(total)
+
+# difference
+a = int (input("Enter first number :"))
+b = int (input("Enter second number"))
+c = a - b
+print ("difference of two", c)
+
+# product
+d = int(input("Enter your first number :"))
+e = int (input("Enter your second number"))
+f = d * e
+print ("product of two number :", f)
+
+#quotient
+g = int (input("Enter your first number:"))
+h = int (input("Enter your second number"))
+i = g / h 
+print ("quotient of two num :", i)
