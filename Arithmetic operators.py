e61eab38a6f3f7742de96c62fb5cd192c957c37a
kollol2 <kollolmondal987654321@gmail.com>
2025-11-21 11:17:43 +0530

    sum

---
+++ Arithmetic operators.py
@@ -0,0 +1,7 @@
+#Take two numbers as input from the user and print their sum, difference, product, and quotient
+
+#sum
+num1 = int (input("Enter your first number :"))
+num2 = int (input("Enter your second number :"))
+total = num1 + num2
+print(total)
